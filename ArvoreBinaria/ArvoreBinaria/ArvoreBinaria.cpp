85fff0a1b9157f6e5155ff0c99d63ae81f3ca834
MarcusBomfim <160621829+MarcusBomfim@users.noreply.github.com>
2024-05-14 10:45:39 -0300

    11 commit

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -78,7 +78,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 
 	cout << "Arvore inicializada \n";
@@ -179,7 +179,22 @@ void exibirElementosArvore(NO* no)
 
 void buscarElementoArvore(NO* no, int valor)
 {
+	if (no == NULL) {
+		cout << "Elemento encontrado" << endl;
+		return;
+	}
 
+	if (no->valor == valor) {
+		cout << "elemento encontrado" << endl;
+		return;
+	}
+
+	if (valor > no->valor) {
+		buscarElementoArvore(no->dir, valor);
+	}
+	else {
+		buscarElementoArvore(no->esq, valor);
+	}
 
 }
 
